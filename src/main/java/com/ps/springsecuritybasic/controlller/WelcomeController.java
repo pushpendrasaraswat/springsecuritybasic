27fad2b7f298027166aaf0403431d82708768778
Pushpendra Saraswat <pushpendra.saraswat2@mercedes-benz.com>
2024-04-28 16:37:08 +0100

    spring basic security

---
+++ src/main/java/com/ps/springsecuritybasic/controlller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.ps.springsecuritybasic.controlller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome(){
+        return "Welcome to spring Application with security.";
+    }
+}
